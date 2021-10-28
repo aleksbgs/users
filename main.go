6b286f1779e41f8f8b0d988f3dd4512913503a24
Aleksandar Markovic <aleksandar.markovic@firstbeatmedia.com>
2021-10-28 23:36:26 +0200

    test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aleksbgs/users/src/database"
+	"github.com/aleksbgs/users/src/routes"
 	"github.com/gofiber/fiber/middleware/cors"
 	"github.com/gofiber/fiber/v2"
 )
